bb9e7e8ef48b56876427c981651a197aa079405f
miss-gif <svx327@naver.com>
2024-05-14 11:53:43 +0900

    Feat : CRUD 간단 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,71 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+const ProductItem = ({ product, onDelete, onUpdate }) => {
+  const { id, name, price, explanation } = product;
+
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editName, setEditName] = useState(name);
+  const [editExplanation, setEditExplanation] = useState(explanation);
+  const [editPrice, setEditPrice] = useState(price);
+
+  return (
+    <div>
+      <div>{id}</div>
+      <div>{name}</div>
+      <div>{price}</div>
+      <div>{explanation}</div>
+
+      <button type="button" onClick={() => onDelete(id)}>
+        삭제하기
+      </button>
+      <button
+        type="button"
+        onClick={() => setIsEditMode(isEditMode => !isEditMode)}
+      >
+        수정하기
+      </button>
+
+      {/* 수정하기 폼 */}
+      {isEditMode && (
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            onUpdate({
+              id,
+              name: editName,
+              explanation: editExplanation,
+              price: editPrice,
+            });
+            setIsEditMode(false); // 수정 후 수정 모드 종료
+          }}
+        >
+          <input
+            type="text"
+            placeholder="상품 이름"
+            value={editName}
+            onChange={e => setEditName(e.target.value)}
+          ></input>
+          <input
+            type="text"
+            placeholder="상품 설명"
+            value={editExplanation}
+            onChange={e => setEditExplanation(e.target.value)}
+          ></input>
+          <input
+            type="number"
+            placeholder="상품 가격"
+            value={editPrice}
+            onChange={e => setEditPrice(parseInt(e.target.value, 10))}
+          ></input>
+          <input type="submit" value="상품 수정하기"></input>
+        </form>
+      )}
+    </div>
+  );
+};
 
 function App() {
+  // 상태관리
   const [products, setProducts] = useState([
     {
       id: 0,
@@ -14,19 +79,36 @@ function App() {
   const [explanation, setExplanation] = useState("");
   const [price, setPrice] = useState(0);
 
-  // console.log(products);
+  const fakeId = useRef(0);
+
+  // 이벤트 핸들러 함수
+  const handleCreate = newProduct => {
+    fakeId.current += 1;
+    setProducts([...products, { ...newProduct, id: fakeId.current }]);
+  };
+  const handleDelete = id => {
+    setProducts(products.filter(product => product.id !== id));
+  };
+  const handleUpdate = updatedProduct => {
+    setProducts(
+      products.map(product =>
+        product.id === updatedProduct.id ? updatedProduct : product,
+      ),
+    );
+  };
 
   return (
     <>
       <form
         onSubmit={e => {
           e.preventDefault();
-          console.log("상품 등록");
+          handleCreate({ name, explanation, price });
         }}
       >
         <input
           type="text"
           placeholder="상품 이름"
+          value={name}
           onChange={e => {
             setName(e.target.value);
           }}
@@ -34,6 +116,7 @@ function App() {
         <input
           type="text"
           placeholder="상품 설명"
+          value={explanation}
           onChange={e => {
             setExplanation(e.target.value);
           }}
@@ -41,6 +124,7 @@ function App() {
         <input
           type="number"
           placeholder="상품 가격"
+          value={price}
           onChange={e => {
             setPrice(parseInt(e.target.value, 10));
           }}
@@ -49,12 +133,12 @@ function App() {
       </form>
 
       {products.map(product => (
-        <div key={product.id}>
-          <div>{product.id}</div>
-          <div>{product.name}</div>
-          <div>{product.price}</div>
-          <div>{product.explanation}</div>
-        </div>
+        <ProductItem
+          key={product.id}
+          product={product}
+          onDelete={handleDelete}
+          onUpdate={handleUpdate}
+        />
       ))}
     </>
   );
