cd6860396a94486639f974bc75436584c14e0439
miss-gif <svx327@naver.com>
2024-05-16 14:55:08 +0900

    Refactor : 컴포넌트화 작업

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
+import ProductCreatePage from "./pages/ProductCreatePage";
 
 function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
+      <Route path="create" element={<ProductCreatePage />} />
       <Route path="/:productId" element={<ProductPage />} />
     </Routes>
   );
---
+++ src/pages/ProductCreatePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ProductCreateForm from "../componemts/create/ProductCreateForm";
+
+const ProductCreatePage = () => {
+  return (
+    <>
+      <div>
+        <h1>상품 등록 페이지입니다.</h1>
+        <ProductCreateForm />
+      </div>
+    </>
+  );
+};
+
+export default ProductCreatePage;
